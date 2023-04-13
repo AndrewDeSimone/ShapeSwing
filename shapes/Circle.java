12cff214896339739fe748db97a97516a78da636
Andrew DeSimone <36091688+Molarcylinder10@users.noreply.github.com>
2023-04-13 17:37:17 -0500

    Add files via upload

---
+++ shapes/Circle.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class Circle extends Oval{
+
+	public Circle(int x, int y, int diameter, String color) {
+		super(x, y, diameter, diameter, color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
